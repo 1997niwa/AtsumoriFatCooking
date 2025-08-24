5fad81d977f459d51bfd9eadf660e9e83c9cb90e
1997niwa <1997niwa@gmail.com>
2025-08-24 14:17:37 +0900

    UIから選択できるところまで

--- AtsuMoriFatCook/MainForm.cs
+++ AtsuMoriFatCook/MainForm.cs
@@ -1,11 +1,11 @@
 using AtsuMoriFatCook.Properties;
-using System.Drawing;
 
 namespace AtsuMoriFatCook
 {
     public partial class MainForm : Form
     {
-        private readonly ListRetention _listRetention;
+
+        private readonly RecipeList _recipeList;
         public MainForm()
         {
             InitializeComponent();
@@ -13,21 +13,39 @@ namespace AtsuMoriFatCook
             this.Icon = Resource.Icon;
             this.Text = Resource.Title;
 
-            _listRetention = new ListRetention();
+            _recipeList = new RecipeList();
 
-            for (int i = 0; i < _listRetention.SettingItems.Count; i++)
+            //TableLayoutColumnStyleCollection columnStyles = _tableLayoutPanel.ColumnStyles;
+            //foreach (ColumnStyle style in columnStyles)
+            //{
+            //    if (style.SizeType == SizeType.Percent)
+            //    {
+            //        style.Width = 15;
+            //    }
+            //}
+
+            TableLayoutRowStyleCollection rowStyles = _tableLayoutPanel.RowStyles;
+            foreach (RowStyle rowStyle in rowStyles)
+            {
+                if (rowStyle.SizeType == SizeType.Absolute)
+                {
+                    rowStyle.Height = 33;
+                }
+            }
+
+            for (int i = 0; i < _recipeList.SettingItems.Count; i++)
             {
                 // 左列にチェックボックス
                 CheckBox cb = new CheckBox();
                 cb.Text = "";  // チェックボックスのテキストは空にして左端に配置のみ
                 cb.CheckAlign = ContentAlignment.MiddleLeft;   // チェックマークを左寄せ
                 cb.Dock = DockStyle.Fill;   // セルにフィットさせる
-                if (_listRetention.SettingItems[i] == 1) cb.CheckState = CheckState.Checked;
+                if ((int)Setting.Default[_recipeList.SettingItems[i].Name] == 1) cb.CheckState = CheckState.Checked;
                 _tableLayoutPanel.Controls.Add(cb, 0, i);
 
                 // 右列にテキストラベルなど
                 Label lbl = new Label();
-                lbl.Text = nameof(_listRetention.SettingItems[i]);
+                lbl.Text = _recipeList.SettingItems[i].Name;
                 lbl.Dock = DockStyle.Fill;
                 lbl.TextAlign = ContentAlignment.MiddleLeft;
                 _tableLayoutPanel.Controls.Add(lbl, 1, i);
@@ -36,10 +54,11 @@ namespace AtsuMoriFatCook
 
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-            for (int i = 0; i < _listRetention.SettingItems.Count; i++)
+            for (int i = 0; i < _recipeList.SettingItems.Count; i++)
             {
-                _listRetention.SettingItems[i] = ((CheckBox)_tableLayoutPanel.GetControlFromPosition(0, i)).Checked ? 1 : 0;
+                Setting.Default[_recipeList.SettingItems[i].Name] = ((CheckBox)_tableLayoutPanel.GetControlFromPosition(0, i)).Checked ? 1 : 0;
             }
+            Setting.Default.Save();
         }
     }
 }
---
+++ AtsuMoriFatCook/RecipeModel.cs
@@ -0,0 +1,54 @@
+﻿using AtsuMoriFatCook.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtsuMoriFatCook
+{
+    internal class RecipeModel
+    {
+        internal string Name { get; private set; }
+        internal int Price { get; private set; }
+        internal int Flour { get; private set; }
+        internal int WholeWheatFlour { get; private set; }
+        internal int Sugar { get; private set; }
+        internal int BrownSugar { get; private set; }
+        internal int TomatoPuree { get; private set; }
+        internal int Potato { get; private set; }
+        internal int Carrot { get; private set; }
+        internal int Tomato { get; private set; }
+        internal int Pumpkin { get; private set; }
+        internal int Apple { get; private set; }
+        internal int Orange { get; private set; }
+        internal int Pear { get; private set; }
+        internal int Peach { get; private set; }
+        internal int Cherry { get; private set; }
+        internal int Coconut { get; private set; }
+
+        //設定名、売値、こむぎこ、ぜんりゅうふん、さとう、こくとう、トマトピューレ、じゃがいも、にんじん、トマト、かぼちゃ、リンゴ、オレンジ、ナシ、モモ、さくらんぼ、ヤシのみ
+        internal RecipeModel(String name, int price, int flour, int wholeWheatFlour, int sugar, int brownSugar, int tomatoPuree, int potato, int carrot, int tomato, int pumpkin, int apple, int orange, int pear, int peach, int cherry, int coconut)
+        {
+            Name = name;
+            Price = price;
+            Flour = flour;
+            WholeWheatFlour = wholeWheatFlour;
+            Sugar = sugar;
+            BrownSugar = brownSugar;
+            TomatoPuree = tomatoPuree;
+            Potato = potato;
+            Carrot = carrot;//8
+            Tomato = tomato;//7
+            Pumpkin = pumpkin;//6
+            Apple = apple;//5
+            Orange = orange;//4
+            Pear = pear;//3
+            Peach = peach;//2
+            Cherry = cherry;//1
+            Coconut = coconut;//0
+        }
+    }
+}
+
+
